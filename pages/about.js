47623eec84275a4b8fe7e9652660b4f90dff4330
AnuwatC <anuwatc@wisesoft.co.th>
2021-05-16 00:15:31 +0700

    first commit next js style component

--- pages/about.js
+++ pages/about.js
@@ -3,13 +3,13 @@ import MainLayout from '../src/components/layouts/MainLayout'
 // import Image from 'next/image'
 // import styles from '../styles/Home.module.css'
 
-export default function Home() {
-  return (
-    <MainLayout>
-      <Head>
-        <title>Home</title>
-      </Head>
-      <h1>Home page</h1>
-    </MainLayout>
-  )
+export default function About() {
+	return (
+		<MainLayout>
+			<Head>
+				<title>About</title>
+			</Head>
+			<h1>About page</h1>
+		</MainLayout>
+	)
 }
--- pages/contact.js
+++ pages/contact.js
@@ -3,13 +3,13 @@ import MainLayout from '../src/components/layouts/MainLayout'
 // import Image from 'next/image'
 // import styles from '../styles/Home.module.css'
 
-export default function Home() {
-  return (
-    <MainLayout>
-      <Head>
-        <title>Home</title>
-      </Head>
-      <h1>Home page</h1>
-    </MainLayout>
-  )
+export default function Contact() {
+	return (
+		<MainLayout>
+			<Head>
+				<title>Contact</title>
+			</Head>
+			<h1>Contact page</h1>
+		</MainLayout>
+	)
 }
